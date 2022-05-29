9ec4a33a8c68594ff24f361e5c834431679cec20
Tobias van de Ven <tobiasvdven@hotmail.com>
2022-05-29 21:46:02 +0200

    Basic DI container setup

--- Packbacker.WPF/App.xaml.cs
+++ Packbacker.WPF/App.xaml.cs
@@ -1,8 +1,31 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
 
 namespace Packbacker.WPF
 {
     public partial class App : Application
     {
+        private readonly ServiceProvider serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new();
+
+            ConfigureServices(services);
+
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<I, A>();
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
     }
 }
